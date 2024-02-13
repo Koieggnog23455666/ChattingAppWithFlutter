c1df99b4ab0431baaf2742a3ebb37effef2a84cf
Krishna <krai100200@gmail.com>
2024-02-13 21:18:34 +0530

    App icon change

--- android/app/src/main/kotlin/com/example/chatting_app_with_firebase/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/chatting_app_with_firebase/MainActivity.kt
@@ -1,6 +1,6 @@
-package com.example.chatting_app_with_firebase
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
+package com.example.chatting_app_with_firebase//package com.example.chatting_app_with_firebase
+//
+//import io.flutter.embedding.android.FlutterActivity
+//
+//class MainActivity: FlutterActivity() {
+//}
